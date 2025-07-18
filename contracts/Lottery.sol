b17c61984ded63e0e42bbf9af29c69e117a54707
NickCherny <nickcherny2@gmail.com>
2025-07-18 23:32:34 +0200

    add chainlink pricefeed for conversion rate

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    constructor() public {
+    constructor(address _priceFeedAddress) public {
         usdEntryFee = 2 * 10 ** 18;
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
     }
     function enter() public payable {
         // require minimum payment. 2$
         players.push(msg.sender);
     }
-    function getEntranceFee() public view returns uint256 {}
+    function getEntranceFee() public view returns (uint256) {}
     function startLottery() public {}
 
     function endLottery() public {}
