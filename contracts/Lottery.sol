9a632038671de93ca75c2defadcfa6b2bba5e119
NickCherny <nickcherny2@gmail.com>
2025-07-18 23:17:09 +0200

    add minimum payment required

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,10 +4,16 @@ pragma solidity ^0.6.6;
 
 contract Lottery {
     address payable[] public players;
+    uint256 public usdEntryFee;
+
+    constructor() public {
+        usdEntryFee = 2 * 10 ** 18;
+    }
     function enter() public payable {
+        // require minimum payment. 2$
         players.push(msg.sender);
     }
-    function getEntranceFee() public {}
+    function getEntranceFee() public view returns uint256 {}
     function startLottery() public {}
 
     function endLottery() public {}
