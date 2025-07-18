5dd4ec2a193d4254a962001d61fa7039b4eac4ae
NickCherny <nickcherny2@gmail.com>
2025-07-18 21:23:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. users can enter lottery with ETH based on a USD fee
+2. an admin will choose when the lottery is over
+3. the lottery will select a random winner
---
+++ contracts/Lottery.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    function enter() public payable {}
+    function getEntranceFee() public {}
+    function startLottery() public {}
+    function endLottery() public {}
+}
