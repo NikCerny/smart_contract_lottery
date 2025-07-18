91027a6905d39cf2bfd74285069ad8c12807bf31
NickCherny <nickcherny2@gmail.com>
2025-07-19 01:16:33 +0200

    implement getEntranceFee function. Add test idea in README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1. users can enter lottery with ETH based on a USD fee
 2. an admin will choose when the lottery is over
 3. the lottery will select a random winner
+
+How do we want to test this?
+
+1. mainnet-fork = quick way
+2. development = with mocks
+3. testnet
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,16 +7,32 @@ contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    mapping(address => uint256) public addressToAmountFunded;
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 2 * 10 ** 18;
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
     }
     function enter() public payable {
-        // require minimum payment. 2$
+        uint256 minimumUSD = 2 * 10 ** 18; // 2 USD
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spend more USD"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
         players.push(msg.sender);
     }
-    function getEntranceFee() public view returns (uint256) {}
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10 ** (18 - 8)); // 8 is the decimals of the price feed in chainlink ETH/USD
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 precision = 1 * 10 ** 18;
+        uint256 costToEnter = (usdEntryFee * precision) / ethPrice;
+        return costToEnter;
+    }
     function startLottery() public {}
 
     function endLottery() public {}
